6fdaebe1be8de88868e352f19b0d6f81316d7718
reshmanair4567 <reshmanair4567@gmail.com>
2019-07-27 22:55:09 -0400

    Cloud DevOps

--- create.sh
+++ create.sh
@@ -1,5 +1,4 @@
-
-#creating a shell script to avoid typing long terminal commands everytime to create CloudFormation templateaws cloudformation create-stack \
+aws cloudformation create-stack \
 --stack-name $1 \
 --template-body file://$2 \
 --parameters file://$3 \
--- update.sh
+++ update.sh
@@ -1,4 +1,3 @@
-// creating a shell script to avoid typing long terminal commands everytime to update CloudFormation template
 aws cloudformation update-stack \
 --stack-name $1 \
 --template-body file://$2 \
